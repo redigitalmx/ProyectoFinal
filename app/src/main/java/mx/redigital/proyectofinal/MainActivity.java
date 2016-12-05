c8693c3fc44e18a90ba8da50ce83b727ebfea8a8
redigitalmx <redigital@gmail.com>
2016-12-05 15:30:15 -0600

    cambio

--- app/src/main/java/mx/redigital/proyectofinal/MainActivity.java
+++ app/src/main/java/mx/redigital/proyectofinal/MainActivity.java
@@ -1,44 +1,172 @@
 package mx.redigital.proyectofinal;
-
-
-
 import android.app.DatePickerDialog;
+import android.content.Intent;
 import android.support.design.widget.TextInputEditText;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.DatePicker;
 import android.widget.TextView;
 
+import java.util.regex.Pattern;
+
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextInputLayout tilNombre;
+    private TextInputLayout tilTelefono;
+    private TextInputLayout tilCorreo;
+    private TextInputLayout tilDescripcion;
 
-   private EditText name;
-   private EditText bdate;
-   private EditText bphone;
-   private EditText email;
-   private EditText description;
-
-   private DatePickerDialog datePickerDialog;
-
-   private String tvNombre;
-   private String tvFecha;
-   private String tvTelefono;
-
+    private EditText etnombre;
+    private EditText ettelefono;
+    private EditText etcorreo;
+    private EditText etdescripcion;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        EditText nombre       = (EditText)findViewById(R.id.etNombre);
-        EditText fecha        = (EditText)findViewById(R.id.etFecha);
-        EditText telefono     = (EditText)findViewById(R.id.etTelefono);
-        EditText email        = (EditText)findViewById(R.id.etEmail);
-        EditText descripcion  = (EditText)findViewById(R.id.etDescripcion);
 
+        tilNombre       = (TextInputLayout) findViewById(R.id.til_nombre);
+        tilTelefono     = (TextInputLayout) findViewById(R.id.til_telefono);
+        tilCorreo       = (TextInputLayout) findViewById(R.id.til_email);
+        tilDescripcion  = (TextInputLayout) findViewById(R.id.til_descripcion);
+
+
+        etnombre        = (EditText) findViewById(R.id.etNomCompleto);
+        ettelefono      = (EditText) findViewById(R.id.etTelefono);
+        etcorreo        = (EditText) findViewById(R.id.etEmail);
+        etdescripcion   = (EditText) findViewById(R.id.etDescripcion);
+        //fpfnac = (DatePicker)  findViewById(R.id.dpFecNac);
+
+        if (getIntent().getStringExtra("validar") != null) {
+            recibirdatos();
+        }
+
+        etnombre.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                tilNombre.setError(null);
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
+
+        ettelefono.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                tilTelefono.setError(null);
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
+        etcorreo.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                esCorreoValido(String.valueOf(s));
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
+
+        Button botonSiguiente = (Button) findViewById(R.id.btnReg);
+        botonSiguiente.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                validarDatos();
+            }
+
+        });
 
     }
+
+    private boolean esNombreValido(String nombre) {
+        Pattern patron = Pattern.compile("^[a-zA-Z ]+$");
+        if (!patron.matcher(nombre).matches() || nombre.length() > 30) {
+            tilNombre.setError("Nombre inválido");
+            return false;
+        } else {
+            tilNombre.setError(null);
+        }
+
+        return true;
+    }
+
+    private boolean esTelefonoValido(String telefono) {
+        if (!Pattern.PHONE.matcher(telefono).matches()) {
+            tilTelefono.setError("Teléfono inválido");
+            return false;
+        } else {
+            tilTelefono.setError(null);
+        }
+
+        return true;
+    }
+
+
+        private boolean esCorreoValido (String correo) {
+
+            return true;
+        }
+
+
+        private void validarDatos(){
+
+            String nombre   =tilNombre.getEditText().getText().toString();
+            String telefono =tilTelefono.getEditText().getText().toString();
+            String correo   =tilCorreo.getEditText().getText().toString();
+
+            boolean a = esNombreValido(nombre);
+            boolean b = esTelefonoValido(telefono);
+            boolean c = esCorreoValido(correo);
+
+        }
+
+
+    private void enviarDatos(){
+
+        Intent intent=new Intent(MainActivity.this,ConfirmarDatos.class);
+        intent.putExtra("nombre",etnombre.getText().toString());
+        intent.putExtra("telefono",ettelefono.getText().toString());
+        intent.putExtra("correo",etcorreo.getText().toString());
+        intent.putExtra("descripcion",etdescripcion.getText().toString());
+        startActivity(intent);
+        finish();
+
 }
 
