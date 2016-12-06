57ded9583615654393e94c31e368ec9d4cc79d79
redigitalmx <redigital@gmail.com>
2016-12-06 12:52:40 -0600

    cambio

--- app/src/main/java/mx/redigital/proyectofinal/ConfirmarDatos.java
+++ app/src/main/java/mx/redigital/proyectofinal/ConfirmarDatos.java
@@ -1,8 +1,13 @@
 package mx.redigital.proyectofinal;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+import android.view.KeyEvent;
 
 public class ConfirmarDatos extends AppCompatActivity {
 
@@ -35,6 +40,38 @@ public class ConfirmarDatos extends AppCompatActivity {
         tvEmail.setText(email);
         tvDescripcion.setText(descripcion);
 
+        //Aqui se declara el Boton de Editar
+        Button botonEditDatos = (Button) findViewById(R.id.btEditarDatos);
+        botonEditDatos.setOnClickListener(new View.OnClickListener(){
 
+            @Override
+            public void onClick(View v){
+               editardatos();
+
+            }
+
+        });
+
+    }
+
+    public boolean onKeyDown(int keyCode, KeyEvent envent) {
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            editardatos();
+        }
+
+        return super.onKeyDown(keyCode, envent);
+    }
+
+
+    private void editardatos(){
+        Toast.makeText(this,"Enviando informacion para editar", Toast.LENGTH_LONG).show();
+        Intent intent =new Intent(ConfirmarDatos.this,MainActivity.class);
+        intent.putExtra("nombre2",tvNombre.getText().toString());
+        intent.putExtra("telefono2",tvTelefono.getText().toString());
+        intent.putExtra("correo2",tvEmail.getText().toString());
+        intent.putExtra("descripcion2",tvDescripcion.getText().toString());
+        intent.putExtra("validar","true");
+        startActivity(intent);
+        finish();
     }
 }
